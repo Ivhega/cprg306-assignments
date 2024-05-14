7cd998ef81615370a0fc19604b7cbda1c0e2e35b
Ivan <53011087+Ivhega@users.noreply.github.com>
2024-05-14 12:33:41 -0600

    week-2 assignment completed
    Step 2 and 3 finished

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2>Ivan Hernandez</h2>
+      <Link href="https://github.com/Ivhega/cprg306-assignments">My GitHub repository</Link>
+    </div>
+  );
+}
