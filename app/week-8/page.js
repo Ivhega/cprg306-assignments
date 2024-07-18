960e3ea295e50ff15d9642349f1fd08d2d5c3a7d
Ivan <ivhega@gmail.com>
2024-07-18 13:03:27 -0600

    className improved

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -25,23 +25,23 @@ export default function SignInPage(){
     //console.dir(user)
 
     return(
-        <main>
-            <header>
+        <main className="flex flex-col items-center justify-center h-screen">
+            <header className="mb-8">
                 <h1 className="text-3xl">Firebase Auth</h1>
             </header>
             { user ? (
-                <div>
+                <div className="text-center">
                     <p>Welcome {user.displayName}</p>
                     <p>{user.email}</p>
-                    <img className="w-8 h-8" src={user.photoURL}></img>
-                    <p>
-                        <Link href="/week-8/shopping-list/">Shopping List</Link>
+                    <img className="w-8 h-8 rounded-full mx-auto mt-4" src={user.photoURL}></img>
+                    <p className="mt-4">
+                        <Link className="text-blue-500 hover:underline" href="/week-8/shopping-list/">Shopping List</Link>
                     </p>
-                    <button onClick={handleSignOut} className="text-lg m-2 hover:underline">Sign Out</button>
+                    <button onClick={handleSignOut} className="text-lg mt-4 text-blue-500 hover:underline">Sign Out</button>
                 </div>
             ) : (
                 <div>
-                    <button onClick={handleSignIn} className="text-lg m-2 hover:underline">Sign In</button>
+                    <button onClick={handleSignIn} className="text-lg mt-4 text-blue-500 hover:underline">Sign In</button>
                 </div>
             ) }
         </main>
